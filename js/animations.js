0a3859337208aacca91d5cb983a7104427363212
87duran <87duran@gmail.com>
2014-10-04 12:03:59 -0600

    latest version

---
+++ js/animations.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	$('#tweet-controls').hide();
+	$('.tweet-compose').on('focus', function() {
+		$('#tweet-controls').show();
+		$('.tweet-compose').css('height', '75px');
+	});
+	var countDown = function() {
+		var keyCount = 140 - $('.tweet-compose').val().length;
+		$('#char-count').text(keyCount);
+		if (keyCount < 10) {
+			$('#char-count').css('color', 'red');
+		}
+		else ($('#char-count').css('color', '#999'));
+		if (keyCount <= 0) {
+			$('#tweet-submit').hide();
+		}
+		else($('#tweet-submit').show());
+	};
+	$('.tweet-compose').keyup(countDown);
+    $('.tweet-compose').change(countDown);
+    
+    $('#tweet-submit').on('click', function() {
+   		var tweetStuff = $('#tweet-content').clone();
+   		$(tweetStuff).find('.tweet-compose').prepend('#stream');
+
+    });
+});
